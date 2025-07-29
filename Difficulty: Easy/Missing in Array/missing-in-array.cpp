d8d7ab37c66dd7b2aa951bd1b4453caa1fa8a00d
Msg05 <158449334+Msg05@users.noreply.github.com>
2025-07-29 17:23:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.cpp
+++ Difficulty: Easy/Missing in Array/missing-in-array.cpp
@@ -2,23 +2,18 @@ class Solution {
   public:
     int missingNum(vector<int>& arr) {
 
-        long long n = (long long)arr.size() + 1; // avoid overflow
-        long long totalSum = n * (n + 1) / 2;
-        long long arraySum = 0;
+        int n = arr.size() + 1;
+        int xor1 = 0, xor2 = 0;
 
-        for(int i = 0; i < arr.size(); i++) 
-        arraySum += arr[i];
+        // XOR of all numbers from 1 to n
+        for (int i = 1; i <= n; i++) 
+        xor1=xor1^i;
 
-        return (int)(totalSum - arraySum);
+        // XOR of all array elements
+        for (int i = 0; i < arr.size(); i++) 
+        xor2=xor2^arr[i];
+
+        return xor1 ^ xor2;
     }
 };
 
-/*
-Example Dry Run:
-Input: arr = [1, 2, 3, 5]
-n = 5
-totalSum = 5 * 6 / 2 = 15
-arraySum = 1+2+3+5 = 11
-
-missing = 15 - 11 = 4 
-*/
