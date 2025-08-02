0797e0b2196c1d4d3153b848482397983f6879a5
Msg05 <158449334+Msg05@users.noreply.github.com>
2025-08-02 23:31:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Mean of an Array/mean-of-an-array.py
@@ -0,0 +1,12 @@
+# User function Template for python3
+
+class Solution:
+    def findMean(self, arr):
+        # Calculate sum of all elements
+        total = sum(arr)
+        
+        # Calculate mean
+        mean = total / len(arr)
+        
+
+        return int(mean)
