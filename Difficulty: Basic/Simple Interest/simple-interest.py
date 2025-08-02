52a6659aadf45581d0ab99a19bf3ed93e83224c3
Msg05 <158449334+Msg05@users.noreply.github.com>
2025-08-02 23:30:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Simple Interest/simple-interest.py
@@ -0,0 +1,8 @@
+# User function Template for python3
+
+class Solution:
+    def simpleInterest(self, A, B, C):
+        # Calculate simple interest
+        SI = (A * B * C) / 100
+        # Return as float (not string)
+        return SI
