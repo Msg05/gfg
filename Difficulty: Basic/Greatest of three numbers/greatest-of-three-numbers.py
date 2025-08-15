17741bef1af07aa4d3cf2b45964cdf6bef5d2668
Msg05 <158449334+Msg05@users.noreply.github.com>
2025-08-15 22:24:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Greatest of three numbers/greatest-of-three-numbers.py
+++ Difficulty: Basic/Greatest of three numbers/greatest-of-three-numbers.py
@@ -1,9 +1,3 @@
 class Solution:
-    def greatestOfThree(self, a, b, c):
-        # Using conditional statements
-        if a >= b and a >= c:
-            return a
-        elif b >= a and b >= c:
-            return b
-        else:
-            return c
+    def greatestOfThree(self, a: int, b: int, c: int) -> int:
+        return max(a, b, c)
