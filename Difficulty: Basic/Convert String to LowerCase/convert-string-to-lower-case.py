3c05f490351d809ef9190d321bc36c2d5004ac52
Msg05 <158449334+Msg05@users.noreply.github.com>
2025-10-13 22:03:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Convert String to LowerCase/convert-string-to-lower-case.py
@@ -0,0 +1,12 @@
+class Solution:
+    def toLower (self , s : str)-> str :
+        #code here 
+        
+        # Convert entire string to lowercase using built-in method
+        return s.lower()
+
+"""
+Explanation:
+Python provides a built-in method .lower() that converts all uppercase letters to lowercase.
+It works efficiently even for large strings (up to 10⁶ characters as per constraints).
+"""
